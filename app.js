d46cd1680d5ef4c34992ed0dadd893f6cb080184
Lorena Guartazaca <lorenaggs2812@gmail.com>
2022-04-11 07:49:03 -0500

    corrections

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
 const btn = document.querySelector(".js_btn");
-const balance = document.queryCommandValue(".js_balance")
+const balance = document.querySelector(".js_balance");
 const points = document.querySelector(".js_points");
 const valor = document.querySelector(".js_valor");
 const numberUser = document.querySelector(".js_numberuser");
@@ -9,43 +9,53 @@ const value = document.querySelector(".js_value");
 const letsGo = document.querySelector(".js_letsgo");
 const validate = document.querySelector(".js_validate");
 
-function pointsValue() {
-  const start = points.value;
-  valor.innerHTML = start;
-}
-  
+let valorInit = 50;
+
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
+}
+
+function win(numAleat, selectValue, balanceAp) {
+  if (selectValue === numAleat) {
+    letsGo.innerHTML = "Has ganado el doble del juego 😀";
+    const balanceWin = balanceAp * 2 + valorInit;
+    balance.innerHTML = balanceWin;
+    if (balanceWin >= 200) {
+      btn.innerHTML = "Reinicio de juego";
+    }
+  }
+}
+
+function lose(numAleat, selectValue, balanceAp) {
+  if (selectValue !== numAleat) {
+    letsGo.innerHTML = "Ha perdido lo apostado ☹️";
+    const resta = valorInit - balanceAp;
+    balance.innerHTML = resta;
+    if (resta < 0) {
+      balance.innerHTML = 0;
+    }
+  }
 }
 
 function play() {
   const numAleat = getRandomNumber(6);
   const selectValue = parseInt(numberUser.value);
-  const valorAcum = points.value * 2;
-  if (selectValue === numAleat) {
-    letsGo.innerHTML = "HA GANADO EL JUEGO";
-    valor.innerHTML = valorAcum;
-  } else {
-    letsGo.innerHTML = "Ha perdido el juego";
-    valor.innerHTML = 0;
-  }
-  validateValue(valorAcum);
+  const balanceAp = parseInt(points.value);
+  win(numAleat, selectValue, balanceAp);
+  lose(numAleat, selectValue, balanceAp);
 }
-
-function validateValue(valorAcum) {
-  if (valorAcum >= 200) {
-    validate.innerHTML = `Su acumulado es de ${valorAcum} ha lelgado al maximo, por favor retirese`;
-  } else {
-    validate.innerHTML = `Siga jugando`;
-  }
-}
-
 
 function handleclickBtn(event) {
   event.preventDefault();
-  pointsValue();
+  balance.innerHTML = valorInit;
+  
   play();
-  // validate();
+
+  if (btn.innerText === "Reinicio de juego") {
+  validate.innerHTML = `Su valor acumulado es de ${balance.innerText} retirese del juego`
+  }
 }
 
-btn.addEventListener('click', handleclickBtn);
+btn.addEventListener("click", handleclickBtn);
+
+ 
